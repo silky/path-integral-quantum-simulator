00939c21035eb7739730b45136726f23ce68d039
Thomas Parks <thomasparks@outlook.com>
2018-05-14 16:52:35 +0100

    parameterize the network layout.

--- src/cpp/model2.cpp
+++ src/cpp/model2.cpp
@@ -137,14 +137,16 @@ struct PrintWorkLists : sc_module {
 
     void thread_process() {
         int i;
-        while (1) {
-            wait(sc_time(10, SC_NS));
-            i = 0;
-            for (int i=0; i<len_worklists; i++) {
-                cout << "----------------worklist " << i << endl;
-                print_worklist(*worklists[i]);
+        if (DEBUG) {
+            while (1) {
+                wait(sc_time(10, SC_NS));
+                i = 0;
+                for (int i=0; i<len_worklists; i++) {
+                    cout << "----------------worklist " << i << endl;
+                    print_worklist(*worklists[i]);
+                }
+                cout << "----iteration end\n\n " << i << endl;
             }
-            cout << "----iteration end\n\n " << i << endl;
         }
     }
 };
@@ -159,7 +161,7 @@ struct Initiator : sc_module {
     state_t target = 0;
     state_t inital = 0;
     int replycount = 0;
-    vector<state_t> requests = {0, 1};
+    vector<state_t> requests = {0, 1, 2, 3};
     
     SC_CTOR(Initiator) : socket("wu_gen") {
         socket.register_nb_transport_bw(this, &Initiator::nb_transport_bw);
@@ -170,7 +172,7 @@ struct Initiator : sc_module {
     void thread_process()
     {
         for (state_t& req : requests) {
-            cout << "making a request" << endl;
+            if (DEBUG) cout << "making a request" << endl;
             tlm::tlm_phase phase;
             sc_time delay;
             
@@ -197,7 +199,7 @@ struct Initiator : sc_module {
     virtual tlm::tlm_sync_enum work_return_fun( tlm::tlm_generic_payload& trans,
                                         tlm::tlm_phase& phase, sc_time& delay ) {
       replycount++;
-      cout << "got a reply!" << endl;
+      if (DEBUG) cout << "got a reply!" << endl;
       workreturn_t *wrecv_ptr = reinterpret_cast<workreturn_t*>(trans.get_data_ptr());
       
       cout << "final amplitude starting at " << StateRepr(inital)
@@ -233,13 +235,13 @@ struct BaseCase : sc_module {
     virtual tlm::tlm_sync_enum ProcessBaseCase( tlm::tlm_generic_payload& trans,
                                         tlm::tlm_phase& phase, sc_time& delay ) {
         
-        cout << "got a base case!" << endl;
+        if (DEBUG) cout << "got a base case!" << endl;
         workunit_t *wu_ptr = reinterpret_cast<workunit_t*>(trans.get_data_ptr());
         
         amp_t amplitude = (wu_ptr->target == wu_ptr->inital) ? (amp_t){1.0, 0.0} : (amp_t){0.0, 0.0};
         
         
-        cout << "returning!" << endl;
+        if (DEBUG) cout << "returning!" << endl;
         tlm::tlm_phase phase_ret;
         sc_time delay_ret;
         
@@ -293,11 +295,11 @@ struct FindAmp : sc_module {
   virtual tlm::tlm_sync_enum recv_wu( tlm::tlm_generic_payload& trans,
                                       tlm::tlm_phase& phase, sc_time& delay ) {
 
-    cout << "got a new request" << endl;
+    if (DEBUG) cout << "got a new request" << endl;
     workunit_t *wu_ptr = reinterpret_cast<workunit_t*>(trans.get_data_ptr());
     workrequest_queue.push(*wu_ptr);
     
-    cout << "enqueued" << endl;
+    if (DEBUG) cout << "enqueued" << endl;
     return tlm::TLM_ACCEPTED;
   }
   
@@ -305,7 +307,7 @@ struct FindAmp : sc_module {
                                       tlm::tlm_phase& phase, sc_time& delay ) {
 
     workreturn_t *wret_ptr = reinterpret_cast<workreturn_t*>(trans.get_data_ptr());
-    cout << "got a reply - [" << wret_ptr->wu_dest << "][" << wret_ptr->wu_dst_pred_idx << "] = " << wret_ptr->amplitude << endl;
+    if (DEBUG) cout << "got a reply - [" << wret_ptr->wu_dest << "][" << wret_ptr->wu_dst_pred_idx << "] = " << wret_ptr->amplitude << endl;
     workreplies_queue.push(*wret_ptr);
 
     // worklist[wret_ptr->wu_dest].amplitudes[wret_ptr->wu_dst_pred_idx] = wret_ptr->amplitude;
@@ -344,7 +346,7 @@ struct FindAmp : sc_module {
 
         if (wu.depth <= mindepth) {
             
-            cout << "reached base state for this block at lvl " << mindepth << endl;
+            if (DEBUG) cout << "reached base state for this block at lvl " << mindepth << endl;
 
             // amp_t amplitude = (wu.target == wu.inital) ? (amp_t){1.0, 0.0} : (amp_t){0.0, 0.0};
             // worklist[wu.wu_dest].amplitudes[wu.wu_dst_pred_idx] = amplitude;
@@ -355,7 +357,7 @@ struct FindAmp : sc_module {
             workunit_req = wu; // copy
             workunit_req.returnloc = REMOTE;
             
-            cout << "making work req from findamp" << endl;
+            if (DEBUG) cout << "making work req from findamp" << endl;
             workrequestor_trans.set_data_ptr( reinterpret_cast<unsigned char*>(&workunit_req) );
             
             phase = tlm::BEGIN_REQ;
@@ -370,7 +372,7 @@ struct FindAmp : sc_module {
             if (wu.returnloc == REMOTE) { // this is the target state we were invoked for
                 // return amplitude;
                 
-                cout << "returning!" << endl;
+                if (DEBUG) cout << "returning!" << endl;
                 
                 workreturn_t *wret = new (workreturn_t){
                                     .target = wu.target,
@@ -419,8 +421,6 @@ struct FindAmp : sc_module {
                                   .wu_dst_pred_idx = i,
                               });
             }
-            // cout << "cant evaluate, TODO" << endl;
-            // break;
         } else { // no work to do.
             // pass! wait for the remote block to update some of our list.
         }
@@ -428,38 +428,179 @@ struct FindAmp : sc_module {
   }
 };
 
+// split the circuit in half for performance reasons! this is where parallelism comes in
+struct HeightDiv : sc_module {
+    // up-circuit connection
+    tlm_utils::simple_target_socket<HeightDiv, sizeof(workunit_t)> up_workrequestor;
+    tlm_utils::simple_initiator_socket<HeightDiv, sizeof(workreturn_t)> up_workreply; 
+    tlm::tlm_generic_payload workreply_trans;
+
+    // down-circuit connection 1
+    tlm_utils::simple_initiator_socket<HeightDiv, sizeof(workunit_t)> down_0_workrequestor;
+    tlm_utils::simple_target_socket<HeightDiv, sizeof(workreturn_t)> down_0_workreply; 
+    tlm::tlm_generic_payload workrequestor_0_trans;
+    
+    // down-circuit connection 2
+    tlm_utils::simple_initiator_socket<HeightDiv, sizeof(workunit_t)> down_1_workrequestor;
+    tlm_utils::simple_target_socket<HeightDiv, sizeof(workreturn_t)> down_1_workreply; 
+    tlm::tlm_generic_payload workrequestor_1_trans;
+
+    SC_CTOR(HeightDiv) : up_workrequestor("up_workrequestor"), up_workreply("up_workreply"),
+                        down_0_workrequestor("down_0_workrequestor"), down_0_workreply("down_0_workreply")
+                        // down_1_workrequestor("down_1_workrequestor"), down_1_workreply("down_1_workreply")
+    {
+        up_workrequestor.register_nb_transport_fw(this, &HeightDiv::WorkRequest_route); 
+        down_0_workreply.register_nb_transport_fw(this, &HeightDiv::WorkReply_route);
+        down_1_workreply.register_nb_transport_fw(this, &HeightDiv::WorkReply_route);
+
+    }
+    
+    virtual tlm::tlm_sync_enum WorkRequest_route( tlm::tlm_generic_payload& trans,
+                                            tlm::tlm_phase& phase, sc_time& delay ) {
+        
+        workunit_t wu = *reinterpret_cast<workunit_t*>(trans.get_data_ptr());
+        if (wu.target.to_ulong() > (1<<NQUBITS)/2) { // upper
+            workrequestor_0_trans.set_data_ptr( reinterpret_cast<unsigned char*>(&wu) );
+            phase = tlm::BEGIN_REQ;
+            down_0_workrequestor->nb_transport_fw( workrequestor_0_trans, phase, delay );
+            return tlm::TLM_ACCEPTED;
+        } else {
+            workrequestor_1_trans.set_data_ptr( reinterpret_cast<unsigned char*>(&wu) );
+            phase = tlm::BEGIN_REQ;
+            down_1_workrequestor->nb_transport_fw( workrequestor_0_trans, phase, delay );
+            return tlm::TLM_ACCEPTED;
+
+        }
+        
+    }
+    
+    virtual tlm::tlm_sync_enum WorkReply_route( tlm::tlm_generic_payload& trans,
+                                            tlm::tlm_phase& phase, sc_time& delay ) {
+        
+        workreturn_t wrt = *reinterpret_cast<workreturn_t*>(trans.get_data_ptr());
+        workreply_trans.set_data_ptr( reinterpret_cast<unsigned char*>(&wrt) );
+        phase = tlm::BEGIN_REQ;
+        up_workreply->nb_transport_fw( workreply_trans, phase, delay );
+        return tlm::TLM_ACCEPTED;
+    }
+};
+
+// layout discr: list of split points + bool of if height split or not.
+// if there is a height split, we need to maintain that split for all sub-things.
+// perhaps be more specific? 
+// explicit tree. basic unit: (findamp, HeightDiv) pair.
+// be super basic:
+enum moduletypes { INITIATOR, FINDAMP, HEIGHTDIV, BASE };
+
+struct amp_connspec {
+    moduletypes type;
+    int idx; 
+};
+
+struct div_connspec {
+    moduletypes type;
+    int idx[2]; 
+};
+static const int n_amp_finders = 3;
+static const int n_divs = 1;
+static const int n_base_cases = 2;
+
+static const int ampdepths[n_amp_finders] = { 3, 0, 0 }; // same length as n_amp_finders
+static const amp_connspec ampconns[n_amp_finders] = { {HEIGHTDIV, 0}, {BASE, 0}, {BASE, 1} };
+static const div_connspec divconns[n_divs] = { {FINDAMP, {1, 2}} };
+
 
 SC_MODULE(Top) {
-  FindAmp *amplitude_finder_late;
-  FindAmp *amplitude_finder_early;
-  PrintWorkLists *printer;
+  FindAmp *amplitude_finders[n_amp_finders];
+  HeightDiv *divs[n_divs];
+  BaseCase *basecases[n_base_cases];
+  
+
   Initiator *init;
-  BaseCase *base;
+  PrintWorkLists *printer;
 
   SC_CTOR(Top) {
     // Instantiate components
+    for (int i=0; i<n_amp_finders; i++) {
+        amplitude_finders[i] = new FindAmp("FindAmp", ampdepths[i]);
+    }
+    for (int i=0; i<n_divs; i++) {
+        divs[i] = new HeightDiv("HeightDiv");
+    }
+    for (int i=0; i<n_base_cases; i++) {
+        basecases[i] = new BaseCase("BaseCase");
+    }
+        
+    vector<workunit_t>* worklists[n_amp_finders];
+    for (int i=0; i<n_amp_finders; i++) {
+        worklists[i] = &amplitude_finders[i]->worklist;
+    }
     
-    
-    amplitude_finder_late = new FindAmp("FindAmp_late", 2);
-    amplitude_finder_early = new FindAmp("FindAmp_early", 0);
-    
-    vector<workunit_t>* worklists[] = { &amplitude_finder_late->worklist, &amplitude_finder_early->worklist };
-
-    printer = new PrintWorkLists("printer", worklists, 2);
+    printer = new PrintWorkLists("printer", worklists, n_amp_finders);
     init = new Initiator("init");
-    base = new BaseCase("basecase");
-    
-    // init to late
-    init->socket.bind(amplitude_finder_late->workrecv);
-    amplitude_finder_late->workret.bind(init->work_return);
-    
-    // late to early plugs
-    amplitude_finder_late->workrequestor.bind(amplitude_finder_early->workrecv);
-    amplitude_finder_early->workret.bind(amplitude_finder_late->workreply);
-    
-    // early to base plug
-    amplitude_finder_early->workrequestor.bind(base->workrecv);
-    base->workret.bind(amplitude_finder_early->workreply);
+    // by only making requests to the top ampfinder, if we request lower than it's cuttoff,
+    // it will process the first workunit whilst distributing the prev levels to lower.
+    // this is slightly dumb but not a problem currently, just a counterintuative layering violation.
+    // we could fix this in findamp by checking the level before enqueue, but this is probably a net loss. alt fix here by requesting to the right module, but this complicates networking needing 1 more port per findamp for no benifit x2.
+    init->socket.bind(amplitude_finders[0]->workrecv);
+    init->work_return.bind(amplitude_finders[0]->workret);
+
+    amp_connspec ampconn;
+    // in Python I would solve this with ducktyping - not sure how to do the dynamic dispatch on a object of unknown type in cpp without the switch?
+    for (int i=0; i<n_amp_finders; i++) {
+        ampconn = ampconns[i];
+        switch (ampconn.type) {
+        case FINDAMP: // direct connection to the next layer down
+            cout << "ampfind direct connection - you might want to merge this!" << endl;
+            amplitude_finders[i]->workrequestor.bind(amplitude_finders[ampconn.idx]->workrecv);
+            amplitude_finders[i]->workreply.bind(amplitude_finders[ampconn.idx]->workret);
+            break;
+        case HEIGHTDIV:
+            amplitude_finders[i]->workrequestor.bind(divs[ampconn.idx]->up_workrequestor);
+            amplitude_finders[i]->workreply.bind(divs[ampconn.idx]->up_workreply);
+            break;
+        case BASE:
+            amplitude_finders[i]->workrequestor.bind(basecases[ampconn.idx]->workrecv);
+            amplitude_finders[i]->workreply.bind(basecases[ampconn.idx]->workret);
+            break;
+        case INITIATOR:
+            cout << "CONNECTION ERROR: tried to make a request to an initator." << endl;
+            break;
+        }
+    }
+ 
+    div_connspec divconn;
+    for (int i=0; i<n_divs; i++) {
+        divconn = divconns[i];
+        switch (divconn.type) {
+        case FINDAMP: // direct connection to the next layer down            
+            divs[i]->down_0_workrequestor.bind(amplitude_finders[divconn.idx[0]]->workrecv);
+            divs[i]->down_0_workreply.bind(amplitude_finders[divconn.idx[0]]->workret);
+            
+            divs[i]->down_1_workrequestor.bind(amplitude_finders[divconn.idx[1]]->workrecv);
+            divs[i]->down_1_workreply.bind(amplitude_finders[divconn.idx[1]]->workret);
+            break;
+        case HEIGHTDIV:
+            cout << "nested divs - you probably don't want this." << endl;
+            divs[i]->down_0_workrequestor.bind(divs[divconn.idx[0]]->up_workrequestor);
+            divs[i]->down_0_workreply.bind(divs[divconn.idx[0]]->up_workreply);
+            
+            divs[i]->down_1_workrequestor.bind(divs[divconn.idx[1]]->up_workrequestor);
+            divs[i]->down_1_workreply.bind(divs[divconn.idx[1]]->up_workreply);
+            break;
+        case BASE:
+            cout << "div to basecase - no point, you don't want this." << endl;
+            divs[i]->down_0_workrequestor.bind(basecases[divconn.idx[0]]->workrecv);
+            divs[i]->down_0_workreply.bind(basecases[divconn.idx[0]]->workret);
+            
+            divs[i]->down_1_workrequestor.bind(basecases[divconn.idx[1]]->workrecv);
+            divs[i]->down_1_workreply.bind(basecases[divconn.idx[1]]->workret);
+            break;
+        case INITIATOR:
+            cout << "CONNECTION ERROR: tried to make a request to an initator. you def dont want this." << endl;
+            break;
+        }
+    }
   }
 
   void print_bw() {

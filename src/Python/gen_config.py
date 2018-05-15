1d8ae55920b5f5261a5a6c928b4cd54f4f96e627
Thomas Parks <thomasparks@outlook.com>
2018-05-15 18:17:24 +0100

    class-global based config generator.

--- src/Python/gen_config.py
+++ src/Python/gen_config.py
@@ -2,6 +2,7 @@
 #     pass
 # 
 
+import sys
 
 class AmpFinder(object):
     
@@ -50,9 +51,10 @@ class HeightDiv(object):
     def addself(self):
         type(self)._list.append(self)
 
-    def __init__(self, left_down_conn, right_down_conn):
+    def __init__(self, splitpt, left_down_conn, right_down_conn):
         self.left = left_down_conn
         self.right = right_down_conn
+        self.splitpt = splitpt
         assert(left_down_conn.type == right_down_conn.type)
         
         self.idx = self.count
@@ -60,7 +62,7 @@ class HeightDiv(object):
         self.addself()
         
     def __str__(self):
-        return "{},{},{}".format( self.left.type, self.left.idx, self.right.idx )
+        return "{},{},{},{}".format(self.splitpt, self.left.type, self.left.idx, self.right.idx )
 
 
 class Base(object):
@@ -81,7 +83,8 @@ class Base(object):
         self.idx = self.count
         self.count += 1
 
-
+def quadSplit(d1, d2, d3, d4):
+    return HeightDiv(8, HeightDiv(4, d1, d2), HeightDiv(12, d3, d4))
 # 
 # class HeightDiv(object):
 #     pass
@@ -89,14 +92,25 @@ class Base(object):
 # class Base(object):
 #     pass
 
-spec = AmpFinder(5, 
-            AmpFinder(3, 
-                HeightDiv(
-                    AmpFinder(0, Base()), 
-                    AmpFinder(0, Base())
-                    )
-                )
+# spec = AmpFinder(int(sys.argv[1]), 
+#             HeightDiv(sys.argv[2],
+#                 AmpFinder(0, Base()), 
+#                 AmpFinder(0, Base())
+#                 )
+#         )
+
+spec = AmpFinder(int(sys.argv[1]),
+            quadSplit(
+                AmpFinder(0, Base()), 
+                AmpFinder(0, Base()), 
+                AmpFinder(0, Base()), 
+                AmpFinder(0, Base())
+            )
         )
+
+
+# spec = AmpFinder(0, Base())
+
 
 config = "\n".join(["{}\n{}\n{}".format(AmpFinder._count, HeightDiv._count, Base._count)] + [str(amp) for amp in AmpFinder._list] + [str(div) for div in HeightDiv._list])
 
